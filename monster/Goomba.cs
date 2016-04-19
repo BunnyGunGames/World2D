074652e7660501f75bad4498d69105401ae43938
Nathan A Haskins <nathan@haskins.it>
2016-04-19 17:44:56 -0500

    starting to refactor

--- monster/Goomba.cs
+++ monster/Goomba.cs
@@ -4,24 +4,21 @@ using System.Collections;
 public class Goomba : MonoBehaviour {
 
   [SerializeField]
-  private LayerMask levelLayer;
+  public int levelLayerNumber;
 
   Rigidbody2D rb2d;
   bool facingLeft;
-  
-     private float topAngle;
-     private float sideAngle; 
+
 
   // Use this for initialization
   void Awake() {
-      rb2d = GetComponent<Rigidbody2D>();
+    rb2d = GetComponent<Rigidbody2D>();
   }
 
-
-	void OnCollisionEnter2D(Collision2D c) {
-    if(c.gameObject.layer == 9) {
+  void OnCollisionEnter2D(Collision2D c) {
+    if(c.gameObject.layer == levelLayerNumber) {
       Vector3 normal = c.contacts[0].normal;
-      
+
       if (normal == Vector3.right)
         facingLeft = false;
       if (normal == Vector3.left)
@@ -30,17 +27,15 @@ public class Goomba : MonoBehaviour {
         print("hit from bottom");
       if (normal == Vector3.down)
         print("hit from top");
-     
+
     }
   }
-	
-	void FixedUpdate () {
+
+  void FixedUpdate () {
     float rb2dy = rb2d.velocity.y;
 
     rb2d.velocity = facingLeft ?
       new Vector2(-1 * 2, rb2dy) :
       new Vector2(1 * 2, rb2dy);
-
   }
-
 }
