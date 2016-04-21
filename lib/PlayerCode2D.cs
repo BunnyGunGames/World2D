a9d606d20e0dd04e38eafe43db702f093ec3b4a6
Nathan A Haskins <nathan@haskins.it>
2016-04-21 08:24:06 -0500

    comitting from drop box sync files on mac, please don't explode

--- lib/PlayerCode2D.cs
+++ lib/PlayerCode2D.cs
@@ -1,105 +1,108 @@
 ﻿using UnityEngine;
 using System.Collections;
+using BunnyGun;
 
-public class PlayerCode2D : MonoBehaviour {
-  public float walkSpeed, jumpPower;
-  public bool SpriteFlipMode;
+namespace BunnyGun {
 
-  bool spriteForward;
-  public float m_FacingRight;
+  public class PlayerCode2D : MonoBehaviour {
+    public float walkSpeed, jumpPower;
+    public bool SpriteFlipMode;
 
-  Animator animator;
-  Rigidbody2D rb2d;
+    bool spriteForward;
+    public float m_FacingRight;
 
-  [SerializeField]
-  private LayerMask m_WhatIsGround;   // A mask determining what is ground to the character
+    Animator animator;
+    Rigidbody2D rb2d;
 
-  private Transform m_GroundCheck;
-  const float k_GroundedRadius = .2f;
-  private bool m_Grounded;
-
-  void Awake(){
     
-    animator = GetComponent<Animator>();
-    rb2d = GetComponent<Rigidbody2D>();
-    m_GroundCheck = transform.Find("GroundCheck");
-    
+    private LayerMask m_WhatIsGround;   
 
-  }
-  void Update () {
-    CheckIfGrounded();
-    HandlePlayerMovement();  
+    private Transform m_GroundCheck;
+    const float k_GroundedRadius = .2f;
+    private bool m_Grounded;
 
-  }
-
-  void CheckIfGrounded() {
-    m_Grounded = false;
-
-    Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-    for (int i = 0; i < colliders.Length; i++)
-    {
-      if (colliders[i].gameObject != gameObject)
-        m_Grounded = true;
-    }
-    animator.SetBool("Grounded", m_Grounded);
-
-  }
-
-  void HandlePlayerMovement(){
-    if (Input.GetButtonDown("Fire1")){
-      JumpPlayer();
+    void Awake(){
+      animator = GetComponent<Animator>();
+      rb2d = GetComponent<Rigidbody2D>();
+      m_GroundCheck = transform.Find("GroundCheck");
+      m_WhatIsGround = World2D.world2D.level;
     }
     
-    if (Input.GetButtonDown("Fire2")){
-      ChopPlayer();
-    }
-    //if the player is idle...
-    if(Input.GetAxisRaw("Horizontal") == 0){      
-      IdlePlayer(); //update animation
+    void Update () {
+      CheckIfGrounded();
+      HandlePlayerMovement();  
+
     }
 
-    //if the player is hitting the stick
-    if(Input.GetAxisRaw("Horizontal") != 0){
-      MovePlayerX(); //move player on x axis
-      SpriteFlip();  //handle x axis flip
+    void CheckIfGrounded() {
+      m_Grounded = false;
+
+      Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+      for (int i = 0; i < colliders.Length; i++)
+      {
+        if (colliders[i].gameObject != gameObject)
+          m_Grounded = true;
+      }
+      animator.SetBool("Grounded", m_Grounded);
+
     }
 
-  }
-  void ChopStop() {
-        transform.Find("Weapon_hands").gameObject.SetActive(false);
-  }
-  void ChopPlayer(){
-    if(m_Grounded) {
-      transform.Find("Weapon_hands").gameObject.SetActive(true);
-      animator.SetTrigger("Chopping");
+    void HandlePlayerMovement(){
+      if (Input.GetButtonDown("Fire1")){
+        JumpPlayer();
+      }
+    
+      if (Input.GetButtonDown("Fire2")){
+        ChopPlayer();
+      }
+      //if the player is idle...
+      if(Input.GetAxisRaw("Horizontal") == 0){      
+        IdlePlayer(); //update animation
+      }
+
+      //if the player is hitting the stick
+      if(Input.GetAxisRaw("Horizontal") != 0){
+        MovePlayerX(); //move player on x axis
+        SpriteFlip();  //handle x axis flip
+      }
+
     }
-  }
-  void JumpPlayer(){
+    void ChopStop() {
+          transform.Find("Weapon_hands").gameObject.SetActive(false);
+    }
+    void ChopPlayer(){
+      if(m_Grounded) {
+        transform.Find("Weapon_hands").gameObject.SetActive(true);
+        animator.SetTrigger("Chopping");
+      }
+    }
+    void JumpPlayer(){
    
-    if (m_Grounded){
-      rb2d.AddForce(new Vector2(0f, jumpPower));
+      if (m_Grounded){
+        rb2d.AddForce(new Vector2(0f, jumpPower));
+      }
+
     }
+    void IdlePlayer(){
+      animator.SetBool("Walking", false);
+    }
+    void MovePlayerX(){
+      animator.SetBool("Walking", true);
+      rb2d.velocity = new Vector2(Input.GetAxis("Horizontal") * walkSpeed, rb2d.velocity.y);
 
-  }
-  void IdlePlayer(){
-    animator.SetBool("Walking", false);
-  }
-  void MovePlayerX(){
-    animator.SetBool("Walking", true);
-    rb2d.velocity = new Vector2(Input.GetAxis("Horizontal") * walkSpeed, rb2d.velocity.y);
-
-  }
-  void SpriteFlip(){
-    if(SpriteFlipMode) {
-      //sprite only flip mode
-      GetComponent<SpriteRenderer>().flipX = Input.GetAxis("Horizontal") < 0 ? true : false;
-    } else {
-      //transform flip mode
-      Vector3 flipVector3 = transform.localScale;
-      flipVector3.x = Input.GetAxisRaw("Horizontal") > 0 ? 1f : -1f;
-      m_FacingRight = flipVector3.x;
-      if (transform.localScale.x != flipVector3.x) {
-        transform.localScale = flipVector3;
+    }
+    void SpriteFlip(){
+      if(SpriteFlipMode) {
+        //sprite only flip mode
+        GetComponent<SpriteRenderer>().flipX = Input.GetAxis("Horizontal") < 0 ? true : false;
+      } else {
+        //transform flip mode
+        Vector3 flipVector3 = transform.localScale;
+        flipVector3.x = Input.GetAxisRaw("Horizontal") > 0 ? 1f : -1f;
+        m_FacingRight = flipVector3.x;
+        if (transform.localScale.x != flipVector3.x) {
+          transform.localScale = flipVector3;
+        }
       }
     }
   }
---
+++ lib/World2D.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using BunnyGun;
+
+namespace BunnyGun {
+
+  public class World2D : MonoBehaviour {
+
+    public static World2D world2D;
+
+    [Header("Layer settings")]
+    [SerializeField] public LayerMask level;
+    [SerializeField] public LayerMask player;
+    [SerializeField] public LayerMask playerDamage;
+
+    //[Space(20)]
+
+    void Awake() {
+      if (world2D == null)
+      {
+        DontDestroyOnLoad(gameObject);
+        world2D = this;
+      } else if (world2D != this){
+        Destroy(gameObject);
+      }
+    }
+    public bool LayerMaskIntMatch(LayerMask layerMask, int i) {
+      return layerMask >> i == 1 ? true : false;
+    }
+    public void Foo() {
+      print("fired fpoo");
+    }
+
+  }  
+}
